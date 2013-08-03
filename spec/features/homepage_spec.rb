04d7d7f365ab41f317743ea44ab36bcabf7a124c
Earl St Sauver <estsauver@gmail.com>
2013-08-02 22:38:44 -0700

    Initial layout/structure

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,14 +1,34 @@
 require 'spec_helper'
 
 describe "Homepage and static pages" do
-  it "It should have the title amazon prime" do 
+  it "should have the title amazon prime" do 
     visit root_path
-    expect(page).to have_title("Amazon Prime")
+    expect(page).to have_title("Prime Directive")
   end
 
   it "should have a logo" do
     visit root_path
-    expect(page).to have_css(".logo")
+    expect(page).to have_css("#logo")
+  end
+
+  it "should have a list of products" do
+    visit root_path
+    expect(page).to have_css("ul.products")
+  end
+
+  it "should have no more than 10 products" do
+    visit root_path
+    within("ul.products") do
+      expect(page).to have_css("li.product", count: 2)
+    end
+  end
+
+  it "should have a featured product" do
+    visit root_path
+    expect(page).to have_css(".product.featured")
+    expect(page).to have_css(".product.featured.title")
+    expect(page).to have_css(".product.featured.description")
+    expect(page).to have_css(".product.featured.price")
   end
 
 end
